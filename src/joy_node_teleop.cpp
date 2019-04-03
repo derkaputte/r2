a336c136ba3cae1978182c975774993dd741d311
derkaputte <screen19@web.de>
2019-04-03 17:25:07 +0200

    Update joy_node_teleop.cpp

--- src/joy_node_teleop.cpp
+++ src/joy_node_teleop.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
+#include <turtlesim/Velocity.h>
 #include <sensor_msgs/Joy.h>
 
 
@@ -10,14 +10,14 @@ public:
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-
+  
   ros::NodeHandle nh_;
 
   int linear_, angular_;
   double l_scale_, a_scale_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
-
+  
 };
 
 
@@ -32,7 +32,7 @@ TeleopTurtle::TeleopTurtle():
   nh_.param("scale_linear", l_scale_, l_scale_);
 
 
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  vel_pub_ = nh_.advertise<turtlesim::Velocity>("turtle1/command_velocity", 1);
 
 
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
@@ -41,10 +41,10 @@ TeleopTurtle::TeleopTurtle():
 
 void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  geometry_msgs::Twist twist;
-  twist.angular.z = a_scale_*joy->axes[angular_];
-  twist.linear.x = l_scale_*joy->axes[linear_];
-  vel_pub_.publish(twist);
+  turtlesim::Velocity vel;
+  vel.angular = a_scale_*joy->axes[angular_];
+  vel.linear = l_scale_*joy->axes[linear_];
+  vel_pub_.publish(vel);
 }
 
 
